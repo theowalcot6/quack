71ca47827303366373004c1685582a919ce95e01
Theo Walcot <theowalcot6@hotmail.co.uk>
2023-12-11 16:36:11 +0000

    New form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,28 +6,21 @@ const Contact = () => {
     return (
         <>
             <Navigation />
-            <form className='contact-form' name="contact" action='/' method="POST" data-netlify="true">
-                <div>
-                    <label htmlFor="name">
-                        Your Name:
-                        <input name='name' type='text' id="name" required />
-                    </label>
-                </div>
-                <div>
-                    <label htmlFor="email">
-                        Your Email:
-                        <input name='email' type='email' id="email" required />
-                    </label>
-                </div>
-                <div>
-                    <label htmlFor="message">
-                        Your Message:
-                        <textarea name='message' id="message" required />
-                    </label>
-                </div>
-                <div>
-                    <button type='submit'>Send</button>
-                </div>
+            <form className='contact-form' name="contact" action='#' method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                <input type="hidden" name="form-name" value="contact" />
+                <label class='label'>
+                    Your Name:
+                    <input type="text" name='name' class='name' required='true' placeholder='Your Name' />
+                </label>
+                <label class='label'>
+                    Your Email:
+                    <input type="email" name="email" class="email" required='true' placeholder='example@gmail.com' />
+                </label>
+                <label class='label'>
+                    Your Message:
+                    <textarea name='message' class="message" required='true' placeholder='message' />
+                </label>
+                <button type="submit">Send</button>
             </form>
         </>
     );
